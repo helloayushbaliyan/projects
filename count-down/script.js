d92ea3789c209a03c42c2348e6eef4177a45ac42
helloayushbaliyan <helloayushbaliyan@gmail.com>
2024-02-03 22:01:57 +0530

    add new project of countdown timer

---
+++ count-down/script.js
@@ -0,0 +1,31 @@
+let enddate = "03 February 2024 9:51 PM";
+let time = document.querySelectorAll(".time");  
+// here input is target
+document.querySelector("#end-date").innerText = enddate;
+
+function clock() {
+    let launchdate = new Date(enddate)
+    let currentdate = new Date()
+    let diffrence = (launchdate - currentdate) / 1000;
+
+    if(diffrence<0) return``
+    time[0].value = Math.floor(diffrence / 3600 / 24)
+    time[1].value = Math.floor((diffrence / 3600) % 24)
+    time[2].value = Math.floor((diffrence / 60) % 60)
+    time[3].value = Math.floor(diffrence % 60) 
+
+}
+// initial call of clock function
+clock();
+
+// 1 day=24 hours
+// 1 hours = 60min
+// 60min=3600 second
+
+
+// here is the function which delay the time of 1s
+setInterval(
+    () => {
+        clock()
+    }
+)
