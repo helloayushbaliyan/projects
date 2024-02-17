edc2446ae2732265ec56635e039a915f7fdaa37c
Ayush Baliyan <helloayushbaliyan@gmail.com>
2024-02-17 23:27:40 +0530

    add image feature oon my weather app

--- weather app/script.js
+++ weather app/script.js
@@ -4,27 +4,28 @@ let input = document.querySelector(".city-name")
 
 
 
-let weather=async () => {
-
+let weather = async () => {
+  
     let name = input.value.toLowerCase()
-
-
+ 
     let url = `${baseurl}${name}&appid=c14ada2ad81a9e0a0dfe7e27378b4afa`
     let promis = await fetch(url)
     let respnse = await promis.json()
     console.log(respnse);
 
-    let temperature = document.querySelector(".temperature").innerHTML = `${Math.round(respnse.main.temp)}°C`
+    let temperature = document.querySelector(".temperature").innerHTML = `${Math.round(respnse.main.temp-273.15)}°C`
     let humidity = document.querySelector(".humidity").innerHTML = `${respnse.main.humidity} %`
-    let windspeed = document.querySelector(".wind-speed").innerHTML = `${Math.round(respnse.wind.speed)} Km/h`
+    let windspeed = document.querySelector(".wind-speed").innerHTML = `${Math.round(respnse.wind.speed*3.6)} Km/h`
     let location = document.querySelector(".location-name").innerHTML = `${respnse.name}`
 
-    // let image=document.querySelector(".image")
-    // image.src=`images / X${respnse.weather[0].main}.png`
-    // let nesrc = document.querySelector
+    let images=document.querySelector(".image")
+    images.src=`images/${respnse.weather[0].main}.png`
+
+    
+
+  
 
 }
-button.addEventListener("click",weather )
-button.addEventListener("click",weather )
+button.addEventListener("click", weather);
 
 
