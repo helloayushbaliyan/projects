e3df2087a8196c27c5002980815509021b69174d
Ayush Baliyan <helloayushbaliyan@gmail.com>
2024-03-01 17:28:49 +0530

    toast notification

---
+++ toast notification/script.js
@@ -0,0 +1,38 @@
+let toastbtn = document.querySelectorAll(".toast-btn");
+let toastbox = document.querySelector(".toast-msg");
+
+toastbtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        let toast = document.createElement("div")
+        if (btn.value === "succes") {
+            toast.innerHTML = `<svg class="w-10 h-10 text-green-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+            <path fill-rule="evenodd" d="M12 2a3 3 0 0 0-2.1.9l-.9.9a1 1 0 0 1-.7.3H7a3 3 0 0 0-3 3v1.2c0 .3 0 .5-.2.7l-1 .9a3 3 0 0 0 0 4.2l1 .9c.2.2.3.4.3.7V17a3 3 0 0 0 3 3h1.2c.3 0 .5 0 .7.2l.9 1a3 3 0 0 0 4.2 0l.9-1c.2-.2.4-.3.7-.3H17a3 3 0 0 0 3-3v-1.2c0-.3 0-.5.2-.7l1-.9a3 3 0 0 0 0-4.2l-1-.9a1 1 0 0 1-.3-.7V7a3 3 0 0 0-3-3h-1.2a1 1 0 01-.7-.2l-.9-1A3 3 0 0 0 12 2Zm3.7 7.7a1 1 0 1 0-1.4-1.4L10 12.6l-1.3-1.3a1 1 0 0 0-1.4 1.4l2 2c.4.4 1 .4 1.4 0l5-5Z" clip-rule="evenodd"/>
+          </svg>
+           succesfull`
+           toast.classList.add("succes")
+        } else if (btn.value === "error") {
+            toast.innerHTML = `<svg class="w-10 h-10 text-red-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+            <path fill-rule="evenodd" d="M8.6 2.6A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4c0-.5.2-1 .6-1.4ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z" clip-rule="evenodd"/>
+          </svg>
+           error`
+           toast.classList.add("error")
+
+        } else if (btn.value === "invalid") {
+            toast.innerHTML = `<svg class="w-10 h-10 text-yellow-400 d" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+            <path fill-rule="evenodd" d="M2 12a10 10 0 1 1 20 0 10 10 0 0 1-20 0Zm11-4a1 1 0 1 0-2 0v5a1 1 0 1 0 2 0V8Zm-1 7a1 1 0 1 0 0 2 1 1 0 1 0 0-2Z" clip-rule="evenodd"/>
+          </svg>
+           invalid`
+           toast.classList.add("invalid")
+
+        }
+        toast.classList.add("toast")
+        toastbox.appendChild(toast)
+        console.log(toast);
+         setTimeout(() => {
+            toast.remove()
+         }, 5000);
+    })
+   
+});
+
+
